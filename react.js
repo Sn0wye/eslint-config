2a58bad1c8909ce1d6713d1b8bd6066879c1db3b
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-10-10 09:11:05 -0300

    release: 1.6

--- react.js
+++ react.js
@@ -5,89 +5,92 @@ module.exports = {
     jest: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'standard',
-    'plugin:prettier/recommended',
-    'prettier',
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "standard",
+    "plugin:prettier/recommended",
+    "prettier",
+    "next/core-web-vitals",
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: ['react', 'jsx-a11y', '@typescript-eslint', 'prettier', 'eslint-plugin-import-helpers'],
+  plugins: [
+    "react",
+    "jsx-a11y",
+    "@typescript-eslint",
+    "prettier",
+    "eslint-plugin-import-helpers",
+  ],
   rules: {
-    'comma-dangle': ['error', {
-      'arrays': 'only-multiline',
-      'objects': 'only-multiline',
-      'imports': 'only-multiline',
-      'exports': 'only-multiline',
-      'functions': 'never'
-  }],
-    'prettier/prettier': [
-      'error',
+    "comma-dangle": [
+      "error",
       {
-        "semi": true,
-        "tabWidth": 2,
-        "singleQuote": true,
-        "jsxSingleQuote": true,
-        "arrowParens": "avoid",
-        "endOfLine": "auto",
-        "bracketSpacing": true,
-        "printWidth": 80,
-        "trailingComma": "none",
-        "quoteProps": "preserve",
-      }
-    ],
-    'space-before-function-paren': 'off',
-    'camelcase': 'warn',
-    'no-use-before-define': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'jsx-a11y/alt-text': [
-      'warn',
-      {
-        elements: ['img'],
-        img: ['Image'],
+        arrays: "only-multiline",
+        objects: "only-multiline",
+        imports: "only-multiline",
+        exports: "only-multiline",
+        functions: "never",
       },
     ],
-    'jsx-a11y/aria-props': 'warn',
-    'jsx-a11y/aria-proptypes': 'warn',
-    'jsx-a11y/aria-unsupported-elements': 'warn',
-    'jsx-a11y/role-has-required-aria-props': 'warn',
-    'jsx-a11y/role-supports-aria-props': 'warn',
-    'import-helpers/order-imports': [
-      'warn',
+    "prettier/prettier": [
+      "error",
       {
-        'newlinesBetween': 'always',
-        'groups': [
-          'module',
-          '/^@shared/',
-          ['parent', 'sibling', 'index']
-        ],
-        'alphabetize': { 'order': 'asc', 'ignoreCase': true }
-      }
+        semi: true,
+        tabWidth: 2,
+        singleQuote: true,
+        jsxSingleQuote: true,
+        arrowParens: "avoid",
+        endOfLine: "auto",
+        bracketSpacing: true,
+        printWidth: 80,
+        trailingComma: "none",
+        quoteProps: "preserve",
+      },
     ],
-    'import/prefer-default-export': 'off',
+    "space-before-function-paren": "off",
+    camelcase: "warn",
+    "no-use-before-define": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "jsx-a11y/alt-text": [
+      "warn",
+      {
+        elements: ["img"],
+        img: ["Image"],
+      },
+    ],
+    "jsx-a11y/aria-props": "warn",
+    "jsx-a11y/aria-proptypes": "warn",
+    "jsx-a11y/aria-unsupported-elements": "warn",
+    "jsx-a11y/role-has-required-aria-props": "warn",
+    "jsx-a11y/role-supports-aria-props": "warn",
+    "import-helpers/order-imports": [
+      "warn",
+      {
+        newlinesBetween: "always",
+        groups: ["module", "/^@shared/", ["parent", "sibling", "index"]],
+        alphabetize: { order: "asc", ignoreCase: true },
+      },
+    ],
+    "import/prefer-default-export": "off",
   },
   settings: {
     react: {
-      version: 'detect',
+      version: "detect",
     },
-    'import/parsers': {
-      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
+    "import/parsers": {
+      [require.resolve("@typescript-eslint/parser")]: [".ts", ".tsx", ".d.ts"],
     },
-    'import/resolver': {
-      'node': {
-        'extensions': [
-          '.js',
-          '.jsx'
-        ]
-      }
-    }
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx"],
+      },
+    },
   },
 };
